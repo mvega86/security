b2c61e2fd53112d40d47e1adb3ecade3fef4f835
Magdiel <mvg140386@gmail.com>
2024-02-13 13:05:57 +0100

    JwtService.java implementado

--- src/main/java/mvega/security/jwt/JwtService.java
+++ src/main/java/mvega/security/jwt/JwtService.java
@@ -1,10 +1,35 @@
 package mvega.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+@Service
 public class JwtService{
 
+    private static final String secret_key="gdfgfdg768f76ds8f8sd6f86sda86fsadhkr6ds876f87sdafhjsdhgbfj";
     public String getToken(UserDetails user) {
-        return null;
+        return getToken(new HashMap<>(), user);
+    }
+
+    private String getToken(HashMap<String,Object> extraClaims, UserDetails user) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret_key);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
