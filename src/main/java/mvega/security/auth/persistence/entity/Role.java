f86b5b96111a1327b810ccc986094e9c14c29da1
Magdiel <mvg140386@gmail.com>
2024-02-13 12:08:58 +0100

    Creados entities y repostirio para los usuario y roles

---
+++ src/main/java/mvega/security/auth/persistence/entity/Role.java
@@ -0,0 +1,26 @@
+package mvega.security.auth.persistence.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users = new ArrayList<>();
+
+}
---
+++ src/main/java/mvega/security/auth/persistence/entity/User.java
@@ -0,0 +1,75 @@
+package mvega.security.auth.persistence.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+    @Column(nullable = false)
+    private String username;
+    String password;
+    String email;
+    String name;
+    String lastname;
+    String country;
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = { @JoinColumn(name = "id_user", referencedColumnName = "uuid") },
+            inverseJoinColumns = { @JoinColumn(name = "id_rol", referencedColumnName = "id") }
+    )
+    @JsonIgnore
+    private List<Role> roles = new ArrayList<>();
+
+    public void addRole(Role role){
+        roles.add(role);
+        role.getUsers().add(this);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().toString()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
